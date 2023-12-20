81fbac36f81b83183f0dcf1fa6f68c641d8e60e8
jinjernot <norindes@gmail.com>
2023-12-20 11:43:09 -0600

    clean code

--- builder.py
+++ builder.py
@@ -1,21 +1,26 @@
 import pandas as pd
 
 def createSheet(xlsx_file):
-    # Read Excel file into a DataFrame
+    """Builds a sheet"""
+
+    # Read Excel
     df = pd.read_excel(xlsx_file, sheet_name='Sheet1', skiprows=3)
 
-    # Remove rows 5 to 31
+    # Remove rows
     df = df.drop(index=range(0, 30))
     
     # Append values from column 7 to the end
-    selected_columns = pd.concat([df[["Container Name", "Container Group"]], df.iloc[:, 7:]], axis=1)
+    selected_columns = pd.concat([df[["Container Name", "Container Group", "Series Value"]], df.iloc[:, 7:]], axis=1)
 
-    # Remove rows with NaN values in selected columns
+    # Remove rows with NaN
     selected_columns = selected_columns.dropna()
 
-    # Add a filter to select only rows where "Container Group" contains "Technical Specifications"
-    selected_columns = selected_columns[selected_columns["Container Group"].str.contains("Technical Specifications", na=False)]
+    # Select rows where "Container Group" contains "Technical Specifications"
+    #selected_columns = selected_columns[selected_columns["Container Group"].str.contains("Technical Specifications", na=False)]
 
-    # Save the selected columns data to a new Excel file
-    output_file = 'output_selected_columns.xlsx'
+    # Remove "Container Group" column
+    #selected_columns = selected_columns.drop("Container Group", axis=1)
+
+    # Save Excel file
+    output_file = 'data.xlsx'
     selected_columns.to_excel(output_file, index=False)
